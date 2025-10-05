2702c02b3ca52e634c65d1452091a7f12c7e4f8c
Madhur Sahu <madhursahu@protonmail.com>
2025-10-05 17:23:29 +0530

    HTTP stream POC

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	ch := getLinesChannel(file)
+
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	buffer := ""
+
+	go func() {
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+
+			if err != nil {
+				if buffer != "" {
+					ch <- buffer
+				}
+
+				if err == io.EOF {
+					close(ch)
+					return
+				}
+
+				log.Fatal(err)
+			}
+
+			buffer += string(data[:n])
+			lines := strings.Split(buffer, "\n")
+
+			for i := 0; i < len(lines)-1; i++ {
+				ch <- lines[i]
+			}
+
+			buffer = lines[len(lines)-1]
+		}
+	}()
+
+	return ch
 }
